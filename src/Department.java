6bb0184f46c90f5316f59a513c2c850047b470fa
ArifAkramShaikMohammad <arifakramshaik@gmail.com>
2024-01-12 10:42:21 -0600

    my commit

---
+++ src/Department.java
@@ -0,0 +1,13 @@
+public abstract class Department {
+
+
+    abstract int getDepartmenSize();
+
+    public static void main(String[] args) {
+
+        SubDepartment sub1 = new SubDepartment();
+
+        System.out.println("The Department Size is "+ sub1.getDepartmenSize());
+
+    }
+}
