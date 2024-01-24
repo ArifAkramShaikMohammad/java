edaa149ed54ea432dfb62821cdbb2b107fd87117
ArifAkramShaikMohammad <156052278+ArifAkramShaikMohammad@users.noreply.github.com>
2024-01-24 10:02:10 -0600

    Delete src directory

--- src/SubDepartment.java
+++
@@ -1,9 +0,0 @@
-public class SubDepartment extends Department {
-
-    @Override
-    int getDepartmenSize() {
-        // TODO Auto-generated method stub
-        int DeptSize = 100;
-        return DeptSize;
-    }
-}
